c81c5f794128d7b3fd9c8b54b97cebf7e729b4ae
ashivamj2105 <54582829+ashivamj2105@users.noreply.github.com>
2022-11-03 00:17:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,60 @@
+let shopItemsData = [
+    {
+        id: "dfnsj",
+        name: "Casual Shirt",
+        price: 200,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    },
+    {
+        id: "dgfdsj",
+        name: "T-Shirt",
+        price: 220,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    },
+    {
+        id: "hgfhfgj",
+        name: "Shirt",
+        price: 240,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    },
+    {
+        id: "mknbds",
+        name: "Office Shirt",
+        price: 260,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    },
+    
+    {
+        id: "absds",
+        name: "Casual Shirt",
+        price: 200,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    },
+    {
+        id: "fsdvhbs",
+        name: "T-Shirt",
+        price: 220,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    },
+    {
+        id: "zhssad",
+        name: "Shirt",
+        price: 240,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    },
+    {
+        id: "mcbdhs",
+        name: "Office Shirt",
+        price: 260,
+        desc: "T-Shirt with sample text printed on it.",
+        img: "./images/img1.jpg"
+    }
+
+]
